d473f6b48ded8e96f6e09d9020499b604e4cfb0e
David Hayes <david@blackbrick.software>
2018-02-27 17:23:43 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Joomla Admin Redirect
+
+This plugin redirects a user to a specific url on login to the administrator site.
---
+++ adminredirect/adminredirect.php
@@ -0,0 +1,34 @@
+<?php defined('_JEXEC') or die;
+
+// http://docs.joomla.org/Plugin/Events/System
+// http://docs.joomla.org/J2.5:Creating_a_System_Plugin_to_augment_JRouter
+// http://docs.joomla.org/J3.x:Creating_a_Plugin_for_Joomla
+//~ 
+//~ function pre($var) { return "<pre>".print_r($var,true)."</pre>"; }
+//~ error_reporting(E_ALL&~E_NOTICE);
+//~ ini_set('display_errors',1);
+
+class PlgUserAdminRedirect extends JPlugin {
+		
+	public function __construct(&$subject, $config) {
+		
+		parent::__construct($subject, $config);
+
+		//~ echo 'subject',pre($subject);
+		//~ echo 'config',pre($config);
+		//~ echo 'paramps',pre($this->params);
+		//~ exit;		
+	}
+
+	public function onUserAfterLogin() {
+
+		$app = JFactory::getApplication(); 
+		$redirecturl = $this->params->get('redirecturl');
+		
+		if (!$app->isAdmin()||empty($redirecturl)) return;
+		
+		//~ echo pre($redirecturl); exit;
+		
+		$app->redirect(JRoute::_($redirecturl,false));
+	}
+}
